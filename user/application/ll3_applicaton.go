67cd19ce4a8e9e6671b31f2e911183f3bd67dd14
bottlenome <bottlenome@gmail.com>
2018-12-16 10:03:11 +0900

    Add ll3 application test

--- user/application/ll3_applicaton.go
+++ user/application/ll3_applicaton.go
@@ -1,6 +1,7 @@
 package user
 
 import (
+	"fmt"
 	"github.com/bottlenome/ll3/system"
 	"github.com/bottlenome/ll3/user"
 )
@@ -19,16 +20,20 @@ func Newll3UserApplication(
 func (l *ll3UserApplication) GetMony(username string) (uint64, uint64, error) {
 	user, err := l.repository.GetByUserName(username)
 	if err != nil {
-		panic(err)
+		return 0, 0, fmt.Errorf("faild to get username by %v : %v", username, err)
 	}
 
 	withdrawRate, err := l.systemRepository.WithdrawRate()
+	if err != nil {
+		return 0, 0, fmt.Errorf("faild to get WithdrawRate: %v", err)
+	}
 	earn := uint64(10*withdrawRate) + 1
 	user.Mony += int64(earn)
 
 	user, err = l.repository.Update(user)
 	if err != nil {
-		panic(err)
+		return 0, 0,
+			fmt.Errorf("Could not update user infomation by %v : %v", user, err)
 	}
 
 	return uint64(user.Mony), earn, err
--- user/application/ll3_applicaton_test.go
+++ user/application/ll3_applicaton_test.go
@@ -1,50 +1,65 @@
 package user
 
 import (
+	"errors"
 	models "github.com/bottlenome/ll3/models"
 	system "github.com/bottlenome/ll3/system"
+	user "github.com/bottlenome/ll3/user"
 	"testing"
 )
 
 type testRepository struct {
+	user.UserRepository
+	FakeGetByUserName func(username string) (*models.User, error)
+	FakeUpdate        func(user *models.User) (*models.User, error)
+	FakeCalcTotalMony func() (total float64, err error)
 }
 
 func (r *testRepository) GetByUserName(username string) (*models.User, error) {
-	u := models.User{
-		UserName: username,
-		Mony:     10,
-	}
-	return &u, nil
+	return r.FakeGetByUserName(username)
 }
 
 func (r *testRepository) Update(user *models.User) (*models.User, error) {
-	u := models.User{
-		UserName: user.UserName,
-		Mony:     user.Mony,
-	}
-	return &u, nil
+	return r.FakeUpdate(user)
+}
+
+func (r *testRepository) CalcTotalMony() (total float64, err error) {
+	return r.FakeCalcTotalMony()
 }
 
 type testSystemRepository struct {
 	system.SystemRepository
+	FakeWithdrawRate func() (rate float32, err error)
 }
 
 func (s *testSystemRepository) WithdrawRate() (rate float32, err error) {
-	return 1.0, nil
-}
-
-func (r *testRepository) CalcTotalMony() (total float64, err error) {
-	return 10.0, nil
+	return s.FakeWithdrawRate()
 }
 
 func TestGetMonyNormal(t *testing.T) {
-	tr := &testRepository{}
-	sr := &testSystemRepository{}
-
-	application := &ll3UserApplication{
-		repository:       tr,
-		systemRepository: sr,
+	tr := &testRepository{
+		FakeGetByUserName: func(username string) (*models.User, error) {
+			u := models.User{
+				UserName: username,
+				Mony:     10,
+			}
+			return &u, nil
+		},
+		FakeUpdate: func(user *models.User) (*models.User, error) {
+			return user, nil
+		},
+		FakeCalcTotalMony: func() (float64, error) {
+			return 10.0, nil
+		},
 	}
+	sr := &testSystemRepository{
+		FakeWithdrawRate: func() (float32, error) {
+			return 1.0, nil
+		},
+	}
+
+	application := Newll3UserApplication(tr, sr)
+
 	total, earn, err := application.GetMony("test_user")
 	if err != nil {
 		t.Fatalf("faild test %#v", err)
@@ -54,5 +69,49 @@ func TestGetMonyNormal(t *testing.T) {
 	}
 	if earn != 11 {
 		t.Errorf("mismatch earn:%d expect:%d", 5, earn)
+	}
+}
+
+func TestGetMonyError(t *testing.T) {
+	getByUserNameError := errors.New("GetByUserName")
+	updateError := errors.New("Update")
+	tr := &testRepository{
+		FakeGetByUserName: func(username string) (*models.User, error) {
+			return nil, getByUserNameError
+		},
+		FakeUpdate: func(user *models.User) (*models.User, error) {
+			return nil, updateError
+		},
+	}
+	withdrawRateError := errors.New("WithdrawRate")
+	sr := &testSystemRepository{
+		FakeWithdrawRate: func() (float32, error) {
+			return 0.0, withdrawRateError
+		},
+	}
+
+	application := Newll3UserApplication(tr, sr)
+	_, _, err := application.GetMony("test_user")
+	if err == nil {
+		t.Errorf("does not occur error GetMony: %v", err)
+	}
+
+	tr.FakeGetByUserName = func(username string) (*models.User, error) {
+		u := models.User{}
+		return &u, nil
+	}
+	application = Newll3UserApplication(tr, sr)
+	_, _, err = application.GetMony("test_user")
+	if err == nil {
+		t.Errorf("does not occur error GetMony: %v", withdrawRateError)
+	}
+
+	sr.FakeWithdrawRate = func() (float32, error) {
+		return 0.1, nil
+	}
+	application = Newll3UserApplication(tr, sr)
+	_, _, err = application.GetMony("test_user")
+	if err == nil {
+		t.Errorf("does not occur error update: %v", err)
 	}
 }
